2e7bcc68d47d2fc04dc74d0b5d547339dbc17e9f
Roman Baranovskii <rom8614@gmail.com>
2024-08-06 19:24:09 +0300

    ref

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { FormProvider } from './context/FormContext';
 import App from './App';
 import 'antd/dist/reset.css';
